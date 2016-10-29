8994bedfea3366ddce61bb83dd03ca796ee65062
DaisyChubb <homm0004@algonquincollege.com>
2016-10-28 20:12:25 -0400

    Testing push

---
+++ app/src/main/java/com/example/ash_lee/triviaquiz/TestActivity.java
@@ -0,0 +1,15 @@
+package com.example.ash_lee.triviaquiz;
+
+/**
+ * Created by Ash-Lee on 2016-10-28.
+ */
+
+public class TestActivity {
+    //This is a test, a what? A test.
+
+    //a what?
+
+    //a test.
+
+    /* Ohhh a test */
+}
