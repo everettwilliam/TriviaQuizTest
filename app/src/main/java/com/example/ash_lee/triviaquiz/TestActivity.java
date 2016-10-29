71d174b4bd24393fceebde14d86b7e0accaf99bf
everettwilliam <hold0052@algonquinlive.com>
2016-10-28 20:18:37 -0400

    Commit for quizzly bits

--- app/src/main/java/com/example/ash_lee/triviaquiz/TestActivity.java
+++ app/src/main/java/com/example/ash_lee/triviaquiz/TestActivity.java
@@ -1,10 +1,13 @@
 package com.example.ash_lee.triviaquiz;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Ash-Lee on 2016-10-28.
  */
 
-public class TestActivity {
+public class TestActivity extends AppCompatActivity{
     //This is a test, a what? A test.
 
     //a what?
@@ -12,4 +15,11 @@ public class TestActivity {
     //a test.
 
     /* Ohhh a test */
+
+    protected void onCreate(Bundle quizzlyBits){
+        super.onCreate(quizzlyBits);
+        setContentView(R.layout.activity_quiz);
+
+
+    }
 }
