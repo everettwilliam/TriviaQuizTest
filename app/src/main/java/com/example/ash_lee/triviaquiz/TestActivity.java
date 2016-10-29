4a5f5728a843f9eea6b135d58fa687680e0c2603
everettwilliam <hold0052@algonquinlive.com>
2016-10-28 20:26:38 -0400

    changed quizzly bits

--- app/src/main/java/com/example/ash_lee/triviaquiz/TestActivity.java
+++ app/src/main/java/com/example/ash_lee/triviaquiz/TestActivity.java
@@ -1,13 +1,10 @@
 package com.example.ash_lee.triviaquiz;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-
 /**
  * Created by Ash-Lee on 2016-10-28.
  */
 
-public class TestActivity extends AppCompatActivity{
+public class TestActivity {
     //This is a test, a what? A test.
 
     //a what?
@@ -16,10 +13,4 @@ public class TestActivity extends AppCompatActivity{
 
     /* Ohhh a test */
 
-    protected void onCreate(Bundle quizzlyBits){
-        super.onCreate(quizzlyBits);
-        setContentView(R.layout.activity_quiz);
-
-
-    }
 }
